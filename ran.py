41565783712feb9eeb68ab7d1186bdb3b26833eb
A <rawipha65@gmail.com>
2025-05-22 00:10:18 +0700

    Add files via upload

--- ran.py
+++ ran.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import pandas as pd
 import random
-from livereload import Server
+# from livereload import Server
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
@@ -117,6 +117,6 @@ def reset(level):
     session.modified = True
     return redirect(url_for('index', level=level))
 
-if __name__ == '__main__':
-    server = Server(app.wsgi_app)
-    server.serve(debug=True)
+# if __name__ == '__main__':
+#     server = Server(app.wsgi_app)
+#     server.serve(debug=True)
